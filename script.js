1be4962664e7c9f38cc3e6a0b05e07cdb361b7a5
Ngi Bujri <nbujri7@gmail.com>
2022-11-30 13:45:54 -0500

    add form

--- script.js
+++ script.js
@@ -2,23 +2,39 @@ const player = (name) => {
   const getName = () => name;
 };
 
-const gameBoard = (() => {})();
+const gameBoard = (() => {
+  let boardArr = new Array(9);
+})();
 
 const displayController = (() => {
   const board = document.querySelector(".game-board");
   const startButton = document.querySelector(".btn-start");
+  const cells = document.querySelectorAll(".cell");
+  let isInPlay = false;
+
   const toggleGameState = () => {
-    const currentState = startButton.textContent.toLowerCase();
-    if (currentState === "start") {
+    if (!isInPlay) {
       startButton.textContent = "Restart";
       board.classList.toggle("inactive");
+      isInPlay = true;
     }
   };
+  // add marker on clicked cell
+  const addMarker = (player) => {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        console.log("clicked");
+      });
+    });
+  };
+
   startButton.addEventListener("click", toggleGameState);
 
   return {
     toggleGameState,
+    addMarker,
   };
 })();
 
 displayController.toggleGameState;
+displayController.addMarker();
